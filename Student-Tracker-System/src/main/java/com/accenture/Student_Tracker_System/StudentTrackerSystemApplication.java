3371e22aa6828b04a53bcd3f71202688c035e056
manthan-modi <manthanmodi2003@gmail.com>
2025-05-19 22:22:46 +0530

    First Commit

---
+++ Student-Tracker-System/src/main/java/com/accenture/Student_Tracker_System/StudentTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.Student_Tracker_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTrackerSystemApplication.class, args);
+	}
+
+}
---
+++ Student-Tracker-System/src/test/java/com/accenture/Student_Tracker_System/StudentTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.Student_Tracker_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
